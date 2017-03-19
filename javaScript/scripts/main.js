f61c6e58b1590584f9274e3e5d30305ddabf5ea4
Cesar Varela <cesarac66@hotmail.com>
2017-03-19 09:30:39 -0600

    Comit inicial hasta antes de trabajar con JAVASCRIPT

---
+++ javaScript/scripts/main.js
@@ -0,0 +1,9 @@
+function add(first, second){
+	return first + second;
+}
+
+
+
+var myList = ['Platano', 'Naranja', 'manzana', add, ['Cesar','Aracely']];
+
+var myList = ['Platano', 'Naranja', 'manzana'];
